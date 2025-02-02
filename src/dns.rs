8f99871d1ed5b6fccc7c2667f20d0f76d984351d
Enfernuz <github.colog@aleeas.com>
2025-02-02 23:01:42 +0400

    codecrafters submit [skip ci]

--- src/dns.rs
+++ src/dns.rs
@@ -17,6 +17,8 @@ pub mod message {
     }
 
     impl Header {
+        // Packet Identifier (ID)
+        // A random ID assigned to query packets. Response packets must reply with the same ID.
         pub fn get_id(&self) -> u16 {
             ((self.id_high as u16) << 8) + self.id_low as u16
         }
@@ -26,6 +28,8 @@ pub mod message {
             self.id_low = (value & 0x00FF) as u8;
         }
 
+        // Query/Response Indicator (QR)
+        // 1 for a reply packet, 0 for a question packet.
         pub fn get_qr(&self) -> bool {
             self.qr_opcode_aa_tc_rd & 0x80 == 0x80
         }
@@ -35,7 +39,18 @@ pub mod message {
             self.qr_opcode_aa_tc_rd = if value { t | 0x80 } else { t };
         }
 
-        pub fn as_bytes(&self) -> [u8; 12] {
+        // Question Count (QDCOUNT)
+        // Number of questions in the Question section.
+        pub fn get_qd_count(&self) -> u16 {
+            ((self.qd_count_high as u16) << 8) + self.qd_count_low as u16
+        }
+
+        pub fn set_qd_count(&mut self, value: u16) {
+            self.qd_count_high = ((value & 0xFF00) >> 8) as u8;
+            self.qd_count_low = (value & 0x00FF) as u8;
+        }
+
+        pub fn encode(&self) -> [u8; 12] {
             [
                 self.id_high,
                 self.id_low,
@@ -50,6 +65,131 @@ pub mod message {
                 self.ar_count_high,
                 self.ar_count_low,
             ]
+        }
+    }
+
+    pub struct Label {
+        content: String,
+    }
+
+    impl Label {
+
+        pub fn new(content: &str) -> Label {
+            Label {
+                content: content.to_owned(),
+            }
+        }
+
+        pub fn get_content(&'_ self) -> &'_ str {
+            &self.content
+        }
+
+        pub fn encode(&self) -> Vec<u8> {
+            let length = self.content.len();
+            assert!(length <= u8::MAX as usize, "Label content's length {} is too big (should be less than or equal to {}).", length, u8::MAX);
+            let mut result: Vec<u8> = Vec::new();
+            result.push(length as u8);
+            result.extend_from_slice(self.content.as_bytes());
+            result
+        }
+    }
+
+    pub struct LabelSequence {
+        labels: Vec<Label>,
+    }
+
+    impl LabelSequence {
+
+        pub fn new(labels: Vec<Label>) -> LabelSequence {
+            LabelSequence {
+                labels: labels,
+            }
+        }
+
+        pub fn get_labels(&'_ mut self) -> &'_ mut Vec<Label> {
+            &mut self.labels
+        }
+
+        pub fn encode(&self) -> Vec<u8> {
+            let mut result: Vec<u8> = Vec::new();
+            self.labels.iter().for_each(|label| result.extend(label.encode().iter()));
+            result.push(b'\0');
+            result
+        }
+    }
+
+    pub struct Question {
+        name: LabelSequence,
+        r#type: u16,
+        class: u16,
+    }
+
+    impl Question {
+
+        pub fn new() -> Question {
+            Question {
+                name: LabelSequence::new(Vec::new()),
+                r#type: 0,
+                class: 0,
+            }
+        }
+
+        pub fn get_name(&'_ mut self) -> &'_ mut LabelSequence {
+            &mut self.name
+        }
+
+        pub fn set_name(&mut self, name: LabelSequence) {
+            self.name = name;
+        }
+
+        pub fn get_type(&self) -> u16 {
+            self.r#type
+        }
+
+        pub fn set_type(&mut self, r#type: u16) {
+            self.r#type = r#type;
+        }
+
+        pub fn get_class(&self) -> u16 {
+            self.class
+        }
+
+        pub fn set_class(&mut self, class: u16) {
+            self.class = class;
+        }
+
+        pub fn encode(&self) -> Vec<u8> {
+            let mut result: Vec<u8> = Vec::new();
+            result.extend(self.name.encode().iter());
+            result.push(((self.r#type & 0xFF00) >> 8) as u8);
+            result.push((self.r#type & 0x00FF) as u8);
+            result.push(((self.class & 0xFF00) >> 8) as u8);
+            result.push((self.class & 0x00FF) as u8);
+            result
+        }
+    }
+
+    pub struct Message {
+        header: Header,
+        question: Option<Question>,
+    }
+
+    impl Message {
+
+        pub fn new(header: Header, question: Option<Question>) -> Message {
+            Message {
+                header: header,
+                question: question,
+            }
+        }
+
+        pub fn encode(&self) -> Vec<u8> {
+            let mut result: Vec<u8> = Vec::new();
+            result.extend_from_slice(&self.header.encode());
+            if let Some(question) = self.question.as_ref() {
+                result.extend(question.encode().iter());
+            }
+            result
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::net::UdpSocket;
 
 mod dns;
 use dns::message::Header;
+use dns::message::Message;
+use dns::message::Question;
+use dns::message::LabelSequence;
+use dns::message::Label;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,11 +19,18 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
+
+                let mut question = Question::new();
+                question.get_name().get_labels().extend([Label::new("codecrafters"), Label::new("io")]);
+
                 let mut header: Header = Header::default();
                 header.set_id(1234);
                 header.set_qr(true);
-                let response = header.as_bytes();
-                dbg!("Bytes: {}", response);
+                header.set_qd_count(1);
+                
+                let message = Message::new(header, Some(question));
+                let response = message.encode();
+                dbg!("Bytes: {}", &response);
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
